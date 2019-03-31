026365280d7a4f5df8c1c8098f81e099c6965f56
Jhonatan Macazana <jhonatan.macazana@utec.edu.pe>
2019-03-31 14:15:39 -0500

    Main Code Added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+int main()
+{
+    int lowJumps, highJumps, testCases;
+    int N, walls[100];
 
-int main() {
-	return EXIT_SUCCESS;
+    scanf("%d", &testCases);
+    for (int i = 1; i <= testCases; i++)
+    {
+        highJumps = 0;
+        lowJumps = 0;
+        scanf("%d", &N);
+
+        for (int j = 0; j < N; j++)
+        {
+            scanf("%d", &walls[j]);
+        }
+        for (int k = 0; k < N-1 ; k++)
+        {
+            if (walls[k] < walls[k + 1])
+                lowJumps++;
+            if (walls[k] > walls[k + 1])
+                highJumps++;
+        }
+        printf("Case %d: %d %d\n", i, lowJumps, highJumps);
+    }
+
+    return 0;
 }
-
